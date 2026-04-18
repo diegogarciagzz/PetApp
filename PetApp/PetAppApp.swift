275c99b01754f1d26e041a9d76aa9edb8551510d
Diego Garcia <a01198976@tec.mx>
2026-04-18 00:37:55 -0600

    FIRST COMMIT SETUP

---
+++ PetApp/PetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAppApp.swift
+//  PetApp
+//
+//  Created by Alumno on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
